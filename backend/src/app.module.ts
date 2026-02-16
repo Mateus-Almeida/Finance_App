ab21487c64373e444947ce91844209c7d5c60594
Mateus-Almeida <mateusalmeida147258@gmail.com>
2026-02-16 11:21:48 -0400

    feat: changes on db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,22 +9,37 @@ import { IncomesModule } from './incomes/incomes.module';
 import { InstallmentsModule } from './installments/installments.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'finance_tracker',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      logging: false,
-    }),
+    TypeOrmModule.forRoot(
+      isProduction
+        ? {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            logging: false,
+          }
+        : {
+            type: 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: parseInt(process.env.DB_PORT ?? '5432', 10),
+            username: process.env.DB_USERNAME || 'postgres',
+            password: process.env.DB_PASSWORD || 'password',
+            database: process.env.DB_NAME || 'finance_tracker',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: false,
+            logging: false,
+          },
+    ),
     AuthModule,
     UsersModule,
     CategoriesModule,
